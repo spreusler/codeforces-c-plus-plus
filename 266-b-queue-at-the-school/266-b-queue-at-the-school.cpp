c018b6388178eea44d3b1f842ccd76a8554b02ac
spreusler <stefan@preusler.de>
2021-03-15 18:43:59 +0100

    266-b-queue-at-the-school added

---
+++ 266-b-queue-at-the-school/266-b-queue-at-the-school.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int n, t;
+    string s, a;
+
+    cin >> n >> t;
+    cin >> s;
+
+    a = s;
+
+    while (t--)
+    {
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i + 1] == 'G' && s[i] == 'B')
+            {
+                a[i] = 'G';
+                a[i + 1] = 'B';
+            }
+        }
+
+    s = a;
+    }
+
+    cout << a << endl;
+}
